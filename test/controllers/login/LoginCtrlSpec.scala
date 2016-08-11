f84214b54d6baebc6b04492880294b0315ec244e
Philipp Wiget <ph.wiget@sunrise.ch>
2016-08-11 08:28:02 +0200

    test with token generator

--- test/controllers/login/LoginCtrlSpec.scala
+++ test/controllers/login/LoginCtrlSpec.scala
@@ -1,19 +1,23 @@
 package controllers.login
 
 import helpers.{RequestHelpers, Sessions}
+import play.api.libs.crypto.{CSRFTokenSigner, CSRFTokenSignerProvider, CookieSignerProvider}
 import play.api.libs.json.Json
 import play.api.test.{FakeRequest, PlaySpecification}
-import play.filters.csrf.CSRF
+import play.filters.csrf.{CSRF, CSRFAddToken}
 
 class LoginCtrlSpec extends PlaySpecification with Sessions with RequestHelpers{
+
+  val signer = application.injector.instanceOf[CSRFTokenSigner]
 
   "Login Controller" should {
 
     "login a user" in {
 
+      val token = signer.generateSignedToken
       val url = controllers.login.routes.LoginCtrl.login().url
 
-      val request = route(application, FakeRequest(POST,url,fakeCSRFHeader,Json.obj("username"->"abc","password"-> "ce")).withCookies(fakeCSRFCookie)).get
+      val request = route(application, FakeRequest(POST,url,CSRFHeader(token),Json.obj("username"->"abc","password"-> "ce")).withCookies(CSRFCookie(token))).get
 
        status(request) must equalTo(200)
 
--- test/helpers/RequestHelpers.scala
+++ test/helpers/RequestHelpers.scala
@@ -9,7 +9,11 @@ trait RequestHelpers {
   val unauthorized = Seq("user" -> "nope")
 
   val fakeCSRFHeader =FakeHeaders(Seq("Csrf-Token" -> "0cf1f9703fc9b5f0e7ef59c493012fcb3c1d2e2b-1469171829785-4e098a4a60eb6cbf5d5dd128"))
+  val CSRFHeader =(token: String) => FakeHeaders(Seq("Csrf-Token" -> token))
+
   val fakeCSRFCookie = Cookie("XSRF-TOKEN","0cf1f9703fc9b5f0e7ef59c493012fcb3c1d2e2b-1469171829785-4e098a4a60eb6cbf5d5dd128")
+  val CSRFCookie = (token: String) => Cookie("XSRF-TOKEN",token)
+
   val basicAuthentication ="Authorization" -> "Basic dGVzdGVyM0B0ZXN0LmNvbTpNQ2tkOUtEb2UwMjAxMw=="
 
 }
